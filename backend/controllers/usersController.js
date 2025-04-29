4df524198e23006f6451bae3783fb76667b901a8
Milos Ursulovic <milosursulovic2696@gmail.com>
2025-04-29 16:21:53 +0200

    Implement add user function

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,8 +1,73 @@
+import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import HealthInstitution from "../models/HealthInstitution.js";
 
 //Add a new user
 export const addUser = async (req, res) => {
   try {
+    const {
+      username,
+      password,
+      firstName,
+      lastName,
+      mail,
+      phone,
+      role,
+      uniqueMasterCitizenNumber,
+    } = req.body;
+
+    // Basic validation (you can add more robust validation)
+    if (
+      !username ||
+      !password ||
+      !firstName ||
+      !lastName ||
+      !mail ||
+      !uniqueMasterCitizenNumber
+    ) {
+      return res.status(400).json({ msg: "Missing required fields." });
+    }
+
+    // Check if username or email or uniqueMasterCitizenNumber already exist
+    const existingUser = await User.findOne({
+      $or: [{ username }, { mail }, { uniqueMasterCitizenNumber }],
+    });
+
+    // Hash the password
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ msg: "User with provided credentials already exists." });
+    }
+
+    // Find or create health institution
+    let healthInstitution = await HealthInstitution.findOne({
+      name: "Zdravstveni centar Negotin, Opšta bolnica Bor",
+    });
+    if (!healthInstitution) {
+      healthInstitution = new HealthInstitution({
+        name: "Zdravstveni centar Negotin, Opšta bolnica Bor",
+      });
+      await healthInstitution.save();
+    }
+
+    // Create and save the new user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      mail,
+      phone,
+      role,
+      uniqueMasterCitizenNumber,
+      healthInstitution: healthInstitution._id,
+    });
+
+    await newUser.save();
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
